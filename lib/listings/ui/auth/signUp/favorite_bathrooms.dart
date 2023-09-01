7cd96497004610c8e9c94d49b4f76841eafa099e
Anton Ch <anton3ch@icloud.com>
2023-08-31 19:44:52 -0500

    initiate the project"

---
+++ lib/listings/ui/auth/signUp/favorite_bathrooms.dart
@@ -0,0 +1,12 @@
+List<String> favoriteBathrooms = [
+  "The cozy bathroom at my grandma's house",
+  "The stylish bathroom at that fancy restaurant",
+  "The clean and well-maintained bathroom at work",
+  "The bathroom with an amazing view from a hotel",
+  "The retro-themed bathroom at a friend's place",
+  "The serene bathroom in a spa I visited",
+  "The quirky bathroom at an art gallery",
+  "The luxurious bathroom in a beachfront resort",
+  "The rustic bathroom in a cabin in the woods",
+  "The high-tech bathroom at a tech conference venue",
+];
